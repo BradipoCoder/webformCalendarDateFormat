c0d49bd412ad6608efd3f37c8be8fb45492b44a6
Vincent <tidalsoul@gmail.com>
2015-11-12 16:26:09 +0100

    init

---
+++ js/webform_calendar_date_format.js
@@ -0,0 +1,66 @@
+(function($) {
+
+    $( document ).ready(function() {
+
+        var mySettings = Drupal.settings.webform_calendar_date_format;
+        //console.log(mySettings);
+
+
+
+        var $webformDatepickerContainer = $('.webform-datepicker');
+        if ($webformDatepickerContainer.length == 0) {
+            return;
+        }
+
+        //@todo: controlla se ce ne sono più di uno
+
+        var $dateSelects = $("select", $webformDatepickerContainer);
+        if ($dateSelects.length == 0) {
+            return;
+        }
+
+        $('<input class="edit-submitted-date form-control" readonly="readonly" />').insertBefore('.webform-datepicker');
+
+        $(".edit-submitted-date").click(function(){
+            $("input[type=image]", $webformDatepickerContainer).focus();
+        });
+
+
+        $dateSelects.change(function() {
+            getFullDate();
+        });
+
+        var getFullDate = function() {
+            var year, month, day= false;
+
+            $.each($dateSelects, function() {
+                if($(this).hasClass("year")) {
+                    year = $(this).val();
+                }
+                if($(this).hasClass("month")) {
+                    month = $(this).val();
+                }
+                if($(this).hasClass("day")) {
+                    day = $(this).val();
+                }
+            });
+
+            if(year && month && day) {
+                $.ajax({
+                    method:"POST",
+                    type:"POST",
+                    url: Drupal.settings.basePath + Drupal.settings.pathPrefix + "webform_calendar_date_format/format",
+                    data:{year:year,month:month,day:day}
+                }).success(function(data) {
+                    if(data['formatted']) {
+                        $(".edit-submitted-date").val(data['formatted']);
+                    } else {
+                        $(".edit-submitted-date").val(day + "/" + month + "/" + year);
+                    }
+                }).error(function(data) {
+                    $(".edit-submitted-date").val(day + "/" + month + "/" + year);
+                });
+            }
+        };
+    });
+})(jQuery);
